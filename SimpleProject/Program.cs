ba4f71c3a1e0cee7d8d9131637b84fb3560f8b3b
sawwere <sawer_13@mail.ru>
2023-12-02 15:14:08 +0300

    Add project

---
+++ SimpleProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProject
+{
+    public class Program
+    {
+        /// <summary>
+        /// Площадь треугольника по трем сторонам
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>Площадь треугольника</returns>
+        public static double SquareBy3Sides(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(666);
+            Console.ReadKey();
+        }
+    }
+}
