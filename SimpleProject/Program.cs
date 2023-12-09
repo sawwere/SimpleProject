d2b1bfd35b0961209c6fc79b07b8454d4089de67
OlegTerV <www.oleg2002ter@mail.ru>
2023-12-09 13:35:43 +0300

    add testing

--- SimpleProject/Program.cs
+++ SimpleProject/Program.cs
@@ -6,25 +6,43 @@ using System.Threading.Tasks;
 
 namespace SimpleProject
 {
-    public class Program
-    {
-        /// <summary>
-        /// Площадь треугольника по трем сторонам
-        /// </summary>
-        /// <param name="a"></param>
-        /// <param name="b"></param>
-        /// <param name="c"></param>
-        /// <returns>Площадь треугольника</returns>
-        public static double SquareBy3Sides(double a, double b, double c)
-        {
-            double p = (a + b + c) / 2;
-            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
-        }
+	public class Program
+	{
+		/// <summary>
+		/// Площадь треугольника по трем сторонам
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <param name="c"></param>
+		/// 
+		/// <returns>Площадь треугольника</returns>
+		public static double SquareBy3Sides(double a, double b, double c)
+		{
+			double p = (a + b + c) / 2;
+			return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+		}
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine(666);
-            Console.ReadKey();
-        }
-    }
+		public static double SquareBySideAndHeight(double a, double h)
+		{
+			return a * h * 0.5;
+		}
+
+		public static double SquareBySidesAndAngle(double a, double b, double angle)
+		{
+			angle = (angle * Math.PI) / 180;
+			return a * b * (Math.Sin(angle)) * 0.5;
+		}
+
+		public static double SquareBySideAndRadius(double a, double b, double c, double r)
+		{
+			double p = (a + b + c) / 2;
+			return p * r;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine();
+			Console.ReadKey();
+		}
+	}
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -4,14 +4,35 @@ using SimpleProject;
 
 namespace UnitTestProject1
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestSquareBy3Sides()
-        {
-            Assert.IsTrue(Math.Abs(6 - Program.SquareBy3Sides(3, 4, 5)) < 1e-8);
-            Assert.IsTrue(Math.Abs(224.71857399867952 - Program.SquareBy3Sides(45, 16, 34)) < 1e-8);
-        }
-    }
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestSquareBy3Sides()
+		{
+			Assert.IsTrue(Math.Abs(6 - Program.SquareBy3Sides(3, 4, 5)) < 1e-8);
+			Assert.IsTrue(Math.Abs(224.71857399867952 - Program.SquareBy3Sides(45, 16, 34)) < 1e-8);
+		}
+
+		[TestMethod]
+		public void TestSquareBySideAndHeight()
+		{
+			Assert.IsTrue(Math.Abs(12 - Program.SquareBySideAndHeight(8, 3)) < 1e-8);
+			Assert.IsTrue(Math.Abs(17.5 - Program.SquareBySideAndHeight(7, 5)) < 1e-8);
+		}
+
+		[TestMethod]
+		public void TestSquareBySidesAndAngle()
+		{
+			Assert.IsTrue(Math.Abs(18 - Program.SquareBySidesAndAngle(8, 9, 30)) < 1e-8);
+			Assert.IsTrue(Math.Abs(24 - Program.SquareBySidesAndAngle(8, 12, 30)) < 1e-8);
+		}
+
+		[TestMethod]
+		public void TestSquareBySideAndRadius()
+		{
+			Assert.IsTrue(Math.Abs(12 - Program.SquareBySideAndRadius(5, 5, 6, 1.5)) < 1e-8);
+			Assert.IsTrue(Math.Abs(6 - Program.SquareBySideAndRadius(3, 4, 5, 1)) < 1e-8);
+		}
+	}
 }
